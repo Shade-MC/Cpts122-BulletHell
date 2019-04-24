6f076d08d1547721ce72b0cf104b21bd6187c369
Damaleus <Glenn.smartt@gmail.com>
2019-04-23 22:38:40 -0700

    character moves, can fire deleting projectiles breaks code

--- BulletHell/BulletHell/main.cpp
+++ BulletHell/BulletHell/main.cpp
@@ -1,8 +1,8 @@
 
 //#include "PA9.h"
 #include "downEnemy.h"	
-
-
+#include <iostream>
+using std::cout;
 #include "PA9.h"
 	
 
@@ -27,6 +27,9 @@ int main()
 		window.getSize().y / 10)), sf::Color::Red,
 		*(new sf::Vector2f((window.getSize().x / 2), ((window.getSize().y) - (window.getSize().y / 12))))));*/
 
+	Projectile *nProj = new Projectile(5.f, sf::Color::Yellow, *(new Vector2f(window.getSize().x / 2, window.getSize().y / 2)), true, *(new Vector2f(0, -10000)));
+	Bullets.push_back(nProj);
+
 	player.setPosition(window.getSize().x / 2, window.getSize().y / 10);
 	player.setScale(.7, .7);
 	window.setFramerateLimit(60);
@@ -41,52 +44,58 @@ int main()
 
 		window.clear();
 		window.draw(player);
+		for (iProjectile = Bullets.begin(); iProjectile != Bullets.end(); ++iProjectile)
+		{
+			window.draw(**iProjectile);
+		}
 		window.display();
-
+		
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 		{
 			if (player.getPosition().x > 0)
 			{
-				player.move(Vector2f(-20, 0.f));
+				player.move(Vector2f(-10, 0.f));
 			}
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 		{
 			if (player.getPosition().x<(window.getSize().x))
 			{
-				player.move(Vector2f(20, 0.f));
+				player.move(Vector2f(10, 0.f));
 			}
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
 		{
 			if (player.getPosition().y > 0)
 			{
-				player.move(Vector2f(0.f, -20));
+				player.move(Vector2f(0.f, -10));
 			}
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
 		{
 			if (player.getPosition().y<(window.getSize().y))
 			{
-				player.move(Vector2f(0.f, 20));
+				player.move(Vector2f(0.f, 10));
 			}
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
 		{
-			Projectile *nProj = new Projectile(5.f, sf::Color::White, *(new Vector2f(window.getSize().x / 2, window.getSize().y / 2)), true, *(new Vector2f(0, .0001)));
+			Projectile *nProj = new Projectile(5.f, sf::Color::Yellow, *(new Vector2f(window.getSize().x / 2, window.getSize().y / 2)), true, *(new Vector2f(0, -5)));
 			Bullets.push_back(nProj);
 		}
+		
 		// Bullet Movement Loop
 		for ( iProjectile = Bullets.begin(); iProjectile != Bullets.end(); ++iProjectile)
 		{
-
+			std::cout << "test2" << std::endl;
 			//gets the velocity of the given projectile and moves it
 			(*iProjectile)->move((*iProjectile)->getVelocity());
-			window.draw(**iProjectile);
 			if (outOfBounds(**iProjectile, window))
 			{
+				std::cout << "Test1" << std::endl;
+
 				pPTemp = (*iProjectile);
-				Bullets.erase(iProjectile);
+				iProjectile = Bullets.erase(iProjectile);
 				delete pPTemp;
 			}
 		}
