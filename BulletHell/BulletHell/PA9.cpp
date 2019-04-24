28258f8eb5eada400761320f717a650bc72603e9
Damaleus <Glenn.smartt@gmail.com>
2019-04-23 18:24:29 -0700

    Trying to get program to run and player movement to work

--- BulletHell/BulletHell/PA9.cpp
+++ BulletHell/BulletHell/PA9.cpp
@@ -1,1 +1,21 @@
 #include "PA9.h"
+
+
+
+bool outOfBounds(sf::Shape &shape, sf::Window &windo)
+{
+	bool OoB =false;
+	float xWin = windo.getSize().x, yWin = windo.getSize().y,
+		xShape = shape.getPosition().x, yShape = shape.getPosition().y;
+
+	if (xShape > xWin || xShape < 0) 
+	{
+		OoB = true;
+	}
+
+	if (yShape > yWin || yShape < 0)
+	{
+		OoB = true;
+	}
+	return OoB;
+}
--- BulletHell/BulletHell/main.cpp
+++ BulletHell/BulletHell/main.cpp
@@ -4,17 +4,22 @@ int main()
 {
 	vector<Projectile *> Bullets;
 	vector<Projectile *>::iterator iProjectile;
+	Projectile *pPTemp;
 
-	vector<sf::VideoMode> modes = sf::VideoMode::getFullscreenModes();
+	//vector<sf::VideoMode> modes = sf::VideoMode::getFullscreenModes();
+	sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
 
-	sf::RenderWindow window(modes[0],"BulletHell",sf::Style::Fullscreen);
-	int resHeight = modes[0].height, resWidth = modes[0].width;
+//	sf::RenderWindow window(modes[0],"BulletHell",sf::Style::Fullscreen);
+	//int resHeight = modes[0].height, resWidth = modes[0].width;
 
-	sf::Texture texture;
-	if (!texture.loadFromFile("Galaga_Fighter.png"))
-		return EXIT_FAILURE;
+	//sf::Texture texture;
+	//if (!texture.loadFromFile("Galaga_Fighter.png"))
+	//	return EXIT_FAILURE;
+	/*sf::Sprite player(texture);*/
 
-	sf::Sprite player(texture);
+	sf::RectangleShape player((*(new sf::Vector2f(window.getSize().x / 12,
+		window.getSize().y / 10)), sf::Color::Red,
+		*(new sf::Vector2f((window.getSize().x / 2), ((window.getSize().y) - (window.getSize().y / 12))))));
 
 	while (window.isOpen())
 	{
@@ -29,6 +34,24 @@ int main()
 		window.draw(player);
 		window.display();
 
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+		{
+			if (player.getPosition().x > 0)
+			{
+				player.move(Vector2f(.0001, 0.f));
+			}
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+		{
+			/*if (player.getPosition().x<window.getSize()-player.getSize)
+			{
+
+			}*/
+		}
+
+
+
+
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
 		{
 			Projectile *nProj = new Projectile(5.f, sf::Color::White, *(new Vector2f(window.getSize().x / 2, window.getSize().y / 2)), true, *(new Vector2f(0, .0001)));
@@ -40,7 +63,12 @@ int main()
 
 			//gets the velocity of the given projectile and moves it
 			(*iProjectile)->move((*iProjectile)->getVelocity());
-			//if((*iProjectile)->ge)
+			if (outOfBounds(**iProjectile, window))
+			{
+				pPTemp = (*iProjectile);
+				Bullets.erase(iProjectile);
+				delete pPTemp;
+			}
 		}
 		 
 	}
